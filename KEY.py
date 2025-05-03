7b307a25225a0c7ce96c5148905ab74a587db127
Alessandro Purcineli <alepurbttf@gmail.com>
2025-05-02 23:48:37 -0300

    first commit

--- KEY.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("credentials.json", "r") as f:
-    creds = json.load(f)
-
-# Corrigir o formato da chave
-private_key = creds["private_key"].replace("\n", "\\n")
-print(private_key)
