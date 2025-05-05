9621430c6d1ab5f7fa698836dd2386728fe8890e
Alessandro Purcineli <alessandro.purcineli@gsinima.com.br>
2025-05-05 17:40:50 -0300

    layout parcela e fatura

--- pages/6_VERSÃO.py
+++ pages/6_VERSÃO.py
@@ -1,14 +1,6 @@
 import streamlit as st
-import gspread
-from google.oauth2.service_account import Credentials
 import pandas as pd
 import streamlit as st
-from datetime import date, timedelta, datetime
-from dateutil.relativedelta import relativedelta
-
-import plotly.express as px
-import numpy as np
-import math
 from LYNCE import verificar_login
 st.set_page_config(layout="wide")
 
