ee532ada1d0bb2ddb8dd07619b317911f3b87bf6
Alessandro Purcineli <alessandro.purcineli@gsinima.com.br>
2025-05-29 23:39:43 -0300

    logo on login page

--- LYNCE.py
+++ LYNCE.py
@@ -16,33 +16,36 @@ def verificar_login(username, password):
 
 # Função para a tela de login
 def tela_login():
-    print(f"O tipo de st é: {type(st)}")
-    st.title("Tela de Login")
-    # Inputs de login
-    username = st.text_input("Usuário")
-    password = st.text_input("Senha", type="password")
+    iamge, logn,cont = st.columns([0.3,0.3,0.4])
+    with iamge:
+        st.image('https://i.ibb.co/xKhjx0ny/lynce-versao.png')
+    with logn:
+        st.title("Tela de Login")
+        # Inputs de login
+        username = st.text_input("Usuário")
+        password = st.text_input("Senha", type="password")
 
-    if st.button("Entrar"):
-        # Verifica o login
-        if verificar_login(username, password):
-            
-            st.session_state.logged_in = True
-            st.session_state.username = username
-            st.success("Login bem-sucedido!")
-            
-            lgnpass = getloginandpasswords()
-            user = lgnpass[lgnpass['LOGIN']==username].index[0]
-            nome = lgnpass['NOME'][user] + " " + lgnpass['SOBRENOME'][user]
-            id_arquivo = lgnpass['ID ARQUIVO'][user]
-            nome_arquivo = lgnpass['ARQUIVO'][user]
-            idioma = lgnpass['IDIOMA'][user]
-            st.session_state.name = nome
-            st.session_state.id = id_arquivo
-            st.session_state.arquivo = nome_arquivo
-            st.session_state.useridioma = idioma
-            st.switch_page('pages/1_SALDOS.py')  # Atualiza para a próxima página
-        else:
-            st.error("Usuário ou senha incorretos!")
+        if st.button("Entrar"):
+            # Verifica o login
+            if verificar_login(username, password):
+                
+                st.session_state.logged_in = True
+                st.session_state.username = username
+                st.success("Login bem-sucedido!")
+                
+                lgnpass = getloginandpasswords()
+                user = lgnpass[lgnpass['LOGIN']==username].index[0]
+                nome = lgnpass['NOME'][user] + " " + lgnpass['SOBRENOME'][user]
+                id_arquivo = lgnpass['ID ARQUIVO'][user]
+                nome_arquivo = lgnpass['ARQUIVO'][user]
+                idioma = lgnpass['IDIOMA'][user]
+                st.session_state.name = nome
+                st.session_state.id = id_arquivo
+                st.session_state.arquivo = nome_arquivo
+                st.session_state.useridioma = idioma
+                st.switch_page('pages/1_SALDOS.py')  # Atualiza para a próxima página
+            else:
+                st.error("Usuário ou senha incorretos!")
 
 
 # Função principal para controlar as páginas
