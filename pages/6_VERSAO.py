f9bf43dece8434fa2e6dcb282fe8eb16079c3f76
Alessandro Purcineli <alessandro.purcineli@gsinima.com.br>
2025-06-18 14:30:59 -0300

    logout system

--- pages/6_VERSAO.py
+++ pages/6_VERSAO.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 import streamlit as st
-from LYNCE import verificar_login, logout
+from LYNCE import verificar_login_cookie_ou_session, logout
 from TRADUTOR import traaducaoapp
 if 'logged_in' not in st.session_state or not st.session_state.logged_in:
     st.markdown('Você precisa fazer <a href="https://lyncefinanceiro.streamlit.app/" target="_self">login</a> primeiro.', unsafe_allow_html=True)
     st.stop()
-
+verificar_login_cookie_ou_session()
 idiomado_do_user = st.session_state.useridioma
 
 
